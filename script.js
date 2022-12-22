9a35f20e1a5734b88ca34bf321980b36023bf887
souravak <souravak211@gmail.com>
2022-12-23 05:08:49 +0530

    tab count error correction

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function hot_edge() {
     // is mobile?
     if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
         // mobile
-        tabCount = 20;
+        tabCount = 30;
     } else {
         // desktop
-        tabCount = 30;
+        tabCount = 40;
     }
     // error adjustment
     tabCount+=10;
